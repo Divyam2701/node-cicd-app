003d5bdc3081ce75360ddde1439a31ac0164cf34
Divyam2701 <divyam.patel2701@gmail.com>
2025-04-03 18:55:28 +0530

    changes

--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Reloading application with zero downtime..."
 if ~/.npm-global/bin/pm2 list | grep -q "node-app"; then
-  ~/.npm-global/bin/pm2 restart node-app --update-env
+  ~/.npm-global/bin/pm2 restart node-app 
 else
   echo "Application not running, skipping reload."
 fi
