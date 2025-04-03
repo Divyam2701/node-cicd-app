4a5345f26d84495dfe75c846e58436c08fd2add0
Divyam2701 <divyam.patel2701@gmail.com>
2025-04-03 17:33:01 +0530

    changes

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -4,6 +4,6 @@ if ~/.npm-global/bin/pm2 list | grep -q "node-app"; then
   echo "Application is already running, skipping start."
 else
   echo "Starting application..."
-  ~/.npm-global/bin/pm2 start app.js.js --name "node-app"
+  ~/.npm-global/bin/pm2 start app.js.js --name "node-app" --update-env
 fi
 ~/.npm-global/bin/pm2 save
